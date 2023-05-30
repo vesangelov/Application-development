f0c2c108b3781ff32a55e90eff7340902d258eae
vesangelov <99607908+vesangelov@users.noreply.github.com>
2023-05-30 08:26:25 +0300

    Add files via upload

---
+++ common/CommonDefines.h
@@ -0,0 +1,19 @@
+#ifndef HARDWARE_ACCELERATION_COMMONDEFINES_H
+#define HARDWARE_ACCELERATION_COMMONDEFINES_H
+
+#include <cstdint>
+
+namespace TextureID{
+    enum ResourceId{
+        LAYER_2,
+        PRESS_KEYS
+    };
+}
+
+namespace FontId {
+    enum FontIdKeys {
+        ANGELINE_VINTAGE_40
+    };
+}
+
+#endif //HARDWARE_ACCELERATION_COMMONDEFINES_H
